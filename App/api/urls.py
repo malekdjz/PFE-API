6c98a1adbc4b51e752f802d847fe47337821719a
malekdjz <malekdjz@gmail.com>
2022-09-17 15:50:05 +0100

    more progress

--- App/api/urls.py
+++ App/api/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from api import views
 urlpatterns = [
     path('patients',views.patients),
-    path('patient/:id',views.patient),
-    path('patient/:id/documents',views.patient_documents),
-    path('patient/:id/document/:id',views.patient_document),
-    path('patient/:id/journals',views.patient_journals),
-    path('patient/:id/journal/:id',views.patient_journal),
+    path('patient/<int:pid>/',views.patient),
+    path('patient/<int:pid>/documents',views.patient_documents),
+    path('patient/<int:pid>/document/<int:did>',views.patient_document),
+    path('patient/<int:pid>/journals',views.patient_journals),
+    path('patient/<int:pid>/journal/<int:jid>',views.patient_journal),
     path('archives',views.archives),
-    path('archives/:id',views.archive),
+    path('archives/<int:aid>/',views.archive),
 ]
--- App/api/views.py
+++ App/api/views.py
@@ -1,10 +1,12 @@
+from xml.dom.minidom import Document
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
-from .models import Patient
-from .serializers import PatientSerializer
+from .models import Patient,Archive,PatientJournal,ExternalDocument
+from .serializers import PatientSerializer,ArchiveSerializer,PatientJournalSerializer,ExternalDocumentSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
 # Create your views here.
 
 @api_view(['GET','POST'])
@@ -16,37 +18,51 @@ def patients(request):
     return
 
 @api_view(['GET','PUT','DELETE'])
-def patient(request):
+def patient(request,pid):
     if request.method == 'GET':
-        return
+        patient = Patient.objects.get(id=pid)
+        serializer = PatientSerializer(patient)
+        return Response(serializer.data)
     elif request.method == 'POST':
         return
     return
 
 @api_view(['GET','POST'])
-def patient_documents(request):
+def patient_documents(request,pid):
     if request.method == 'GET':
-        return
+        documents = ExternalDocument.objects.filter(patient_id=pid)
+        serializer = ExternalDocumentSerializer(documents,many=True)
+        return Response(serializer.data)
     return
 
 @api_view(['GET'])
-def patient_document(request):
-    return
+def patient_document(request,pid,did):
+    document = ExternalDocument.objects.get(patient_id=pid,id=did)
+    serializer = ExternalDocumentSerializer(document)
+    return Response(serializer.data)
 
 @api_view(['GET','POST'])
-def patient_journals(request):
+def patient_journals(request,pid):
     if request.method == 'GET':
-        return
+        journals = PatientJournal.objects.filter(patient_id=pid)
+        serializer = PatientJournalSerializer(journals,many=True)
+        return Response(serializer.data)
     return
 
 @api_view(['GET'])
-def patient_journal(request):
-    return
+def patient_journal(request,pid,jid):
+    journal = PatientJournal.objects.get(patient_id=pid,id=jid)
+    serializer = PatientJournalSerializer(journal)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def archives(request):
-    return
+    archives = Archive.objects.all()
+    serializer = ArchiveSerializer(archives,many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
-def archive(request):
-    return
+def archive(request,aid):
+    archive = Archive.objects.get(id=aid)
+    serializer = ArchiveSerializer(archive)
+    return Response(serializer.data)
